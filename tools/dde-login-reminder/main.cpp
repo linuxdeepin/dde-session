c4d29f496b39657fa900cd713d524f9e0d55c7c7
范朋程 <fanpengcheng@uniontech.com>
2022-11-09 13:49:57 +0800

    chore: 减少loginreminder进程驻留时长
    改善机制，避免进程长时间驻留
    
    Log: 减少loginreminder进程驻留时长
    Influence: 后台进程只会一闪而过，不存在长时间驻留的情况
    Task: https://pms.uniontech.com/task-view-203055.html
    Change-Id: I58036de64d4e132537795aef7183809bc0f1af70

--- tools/dde-login-reminder/main.cpp
+++ tools/dde-login-reminder/main.cpp
@@ -1,14 +1,107 @@
-#include <QCoreApplication>
+#include <QDateTime>
+#include <QDBusInterface>
+#include <QDBusPendingReply>
+#include <QNetworkInterface>
 
-#include "loginremindermanager.h"
+#include <unistd.h>
+
+#include "utils.h"
+#include "loginreminderinfo.h"
+
+
+int start()
+{
+    qDebug() << "login reminder init";
+
+    if (!Utils::SettingValue("com.deepin.dde.startdde", QByteArray(), "login-reminder", false).toBool())
+        return 0;
+
+    // 登录后展示横幅通知信息
+#ifdef QT_DEBUG // V20 验证
+    QDBusInterface userInter("com.deepin.daemon.Accounts", QString("/com/deepin/daemon/Accounts/User%1").arg(getuid()), "com.deepin.daemon.Accounts.User", QDBusConnection::systemBus());
+#else
+    QDBusInterface userInter("org.deepin.daemon.Accounts1", QString("/org/deepin/daemon/Accounts1/User%1").arg(getuid()), "org.deepin.daemon.Accounts1.User", QDBusConnection::systemBus());
+#endif
+    QDBusPendingReply<LoginReminderInfo> reply = userInter.call("GetReminderInfo");
+    if (reply.isError()) {
+        qWarning() << "failed to retrieve login reminder info, error: " << reply.error().message();
+        return -1;
+    }
+
+    // 获取当前设备的ip
+    auto getFirstIpAddress = [] {
+        QList<QHostAddress> addressesList = QNetworkInterface::allAddresses();
+
+        // 优先查找ipv4的地址
+        auto ipV4Iter = std::find_if(addressesList.begin(), addressesList.end(), [ = ] (QHostAddress address) {
+            return (address.protocol() == QAbstractSocket::IPv4Protocol)
+                    && (address != QHostAddress::LocalHost)
+                    && (address != QHostAddress::Broadcast)
+                    && (address != QHostAddress::AnyIPv4)
+                    && (address != QHostAddress::Any);
+        });
+
+        if (ipV4Iter != addressesList.end()) {
+            return ipV4Iter->toString();
+        }
+
+        // 其次,寻找可用的ipv6地址
+        auto ipV6Iter = std::find_if(addressesList.begin(), addressesList.end(), [ = ] (QHostAddress address) {
+            return (address.protocol() == QAbstractSocket::IPv6Protocol)
+                    && (address != QHostAddress::LocalHostIPv6)
+                    && (address != QHostAddress::AnyIPv6)
+                    && (address != QHostAddress::Any);
+        });
+
+        if (ipV6Iter != addressesList.end()) {
+            return ipV6Iter->toString();
+        }
+
+        return QString();
+    };
+
+#define SECONDS_PER_DAY (60 * 60 * 24)
+    const LoginReminderInfo &info = reply.value();
+
+    const QString &title = QObject::tr("Login Reminder");
+    const QString &currentLoginTime = info.CurrentLogin.Time.left(QString("yyyy-MM-dd hh:mm:ss").length());
+    const QString &address = (info.CurrentLogin.Address == "0.0.0.0") ? getFirstIpAddress() : info.CurrentLogin.Address;
+    QString body = QString("%1 %2 %3").arg(info.Username).arg(address).arg(currentLoginTime);
+    int daysLeft = info.spent.LastChange + info.spent.Max - int(QDateTime::currentDateTime().toTime_t() / SECONDS_PER_DAY);
+    if ((info.spent.Max != -1) && (info.spent.Warn != -1) && (info.spent.Warn > daysLeft)) {
+        body += " " + QString(QObject::tr("Your password will expire in %1 days")).arg(daysLeft);
+    }
+    body += "\n" + QString(QObject::tr("%1 login failures since the last successful login")).arg(info.FailCountSinceLastLogin);
+
+    const QString &lastLoginTime = info.LastLogin.Time.left(QString("yyyy-MM-dd hh:mm:ss").length());
+    QString content;
+    content += QString("<p>%1</p>").arg(info.Username);
+    content += QString("<p>%1</p>").arg(address);
+    content += "<p>" + QString(QObject::tr("Login time: %1")).arg(currentLoginTime) + "</p>";
+    content += "<p>" + QString(QObject::tr("Last login: %1")).arg(lastLoginTime) + "</p>";
+    content += "<p><b>" + QString("Your password will expire in %1 days").arg(daysLeft) + "</b></p>";
+    content += "<br>";
+    content += "<p>" + QString("%1 login failures since the last successful login").arg(info.FailCountSinceLastLogin) + "</p>";
+
+    QDBusInterface *notifyInter = new QDBusInterface("org.freedesktop.Notifications", "/org/freedesktop/Notifications", "org.freedesktop.Notifications");
+    QVariantMap hints;
+    hints.insert(QString("x-deepin-action-details"),QVariant(QString("/usr/bin/dde-hints-dialog, %1, %2").arg(title).arg(content)));
+    QDBusPendingReply<uint> notifyIdReply = notifyInter->call("Notify", "dde-control-center", uint(0), "preferences-system", title, body, QStringList() << "details" << QObject::tr("Details"), hints, int(0));
+    if (notifyIdReply.isError()) {
+        qWarning() << "failed to call notification, error: " << notifyIdReply.error().message();
+        return -1;
+    }
+
+    qDebug() << "login reminder init finished";
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication app(argc, argv);
-    app.setOrganizationName("deepin");
-    app.setApplicationName("dde-login-reminder");
+    Q_UNUSED(argc);
+    Q_UNUSED(argv);
 
-    LoginReminderManager m;
-    m.init();
-    return app.exec();
+    registerLoginReminderInfoMetaType();
+
+    return start();
 }
