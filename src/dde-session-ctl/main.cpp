6ee351485eb499d89e1f29f1fd115abd4a5227c8
tanfeng <tanfeng@uniontech.com>
2023-01-31 13:38:44 +0800

    fix: 大版本升级异常退出登录问题修复
    V20到V23的升级，重启前两个环境共存会冲突导致登录退出，通过startdde判断V20运行环境。
    该修复只是针对发现的一个异常，冲突实际还会有其他异常，后续需要机制来处理这种升级带来的问题。
    
    Log: 大版本升级异常退出登录问题修复
    Influence: 升级
    Bug: https://pms.uniontech.com/bug-view-183785.html
    Change-Id: I84ec57099857a857ec8a9487991c4cf496048cef

--- src/dde-session-ctl/main.cpp
+++ src/dde-session-ctl/main.cpp
@@ -2,8 +2,58 @@
 #include <QCommandLineParser>
 #include <QDebug>
 #include <QDBusInterface>
+#include <QProcess>
+#include <QVector>
+#include <QDBusObjectPath>
+#include <QDBusVariant>
 
 #include "Session.h"
+
+QVector<uint> getStartddePID() 
+{
+    QProcess process;
+    process.setProgram("pidof");
+    QStringList argument("startdde");
+    process.setArguments(argument);
+    process.start();
+    process.waitForFinished();
+    QStringList pids = QString::fromLocal8Bit(process.readAllStandardOutput()).simplified().split(" ");
+    QVector<uint> ret;
+    for (auto pid : pids) {
+        if (!pid.isEmpty()) {
+            ret.append(pid.toUInt());
+        }
+    }
+    qDebug() << "Startdde Pids:" << ret;
+    return ret;
+}
+
+bool checkStartddeSession()
+{
+    QVector<uint> pids = getStartddePID();
+    for (uint pid : pids) {
+        QDBusInterface systemd("org.freedesktop.systemd1", "/org/freedesktop/systemd1", "org.freedesktop.systemd1.Manager", QDBusConnection::systemBus());
+        QDBusReply<QDBusObjectPath> reply = systemd.call("GetUnitByPID", pid);
+        if (!reply.isValid()) {
+            continue;
+        }
+        qInfo() << "startdde unit:" << reply.value().path();
+        if (reply.value().path().isEmpty()) {
+            continue;
+        }
+        QDBusInterface unit("org.freedesktop.systemd1", reply.value().path(), "org.freedesktop.DBus.Properties", QDBusConnection::systemBus());
+        QDBusReply<QDBusVariant> replyId = unit.call("Get", "org.freedesktop.systemd1.Unit", "Id");
+        if (!replyId.isValid()) {
+            continue;
+        }
+        QString id = replyId.value().variant().toString();
+        if (!id.endsWith("service")) {
+            qWarning() << "check startdde error: is not a service.";
+            return false;
+        }
+    }
+    return true;
+}
 
 int main(int argc, char *argv[])
 {
@@ -43,8 +93,11 @@ int main(int argc, char *argv[])
     }
 
     if (isSessionExit) {
-        QDBusInterface systemd("org.freedesktop.systemd1", "/org/freedesktop/systemd1", "org.freedesktop.systemd1.Manager");
-        qInfo() << systemd.call("StopUnit", "dbus.service", "replace-irreversibly");
+        qInfo() << "session exit...";
+        if (checkStartddeSession()) {
+            QDBusInterface systemd("org.freedesktop.systemd1", "/org/freedesktop/systemd1", "org.freedesktop.systemd1.Manager");
+            qInfo() << systemd.call("StopUnit", "dbus.service", "replace-irreversibly");
+        }
         return 0;
     }
 
