5322494a0a6792f9b204a3f4e7c615bae1824576
Felix Yan <felixonmars@archlinux.org>
2023-09-17 16:08:53 +0300

    chore: correct typos in environmentsmanager.{cpp,h}
    Log:

--- src/dde-session/environmentsmanager.h
+++ src/dde-session/environmentsmanager.h
@@ -18,7 +18,7 @@ public:
     void init();
 
 private:
-    void createGenernalEnvironments();
+    void createGeneralEnvironments();
     void createKeyringEnvironments();
     void createDBusEnvironments();
 
