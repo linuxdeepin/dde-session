259ef6fd35c674d7a4dbfd55559059e621585aa2
Felix Yan <felixonmars@archlinux.org>
2023-11-27 11:51:37 +0200

    chore: correct a typo in main.cpp
    Log:

--- src/dde-session/main.cpp
+++ src/dde-session/main.cpp
@@ -138,7 +138,7 @@ int main(int argc, char *argv[])
             watcher->start();
         });
     } else {
-        qInfo() << "iowait watcher diabled";
+        qInfo() << "iowait watcher disabled";
     }
 
     QtConcurrent::run([&session](){
